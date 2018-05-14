8e2b5d8e34a475010bbfeeae165afa621ade13dc
Nick Muller <muller_nicky@hotmail.com>
2018-05-14 10:41:40 +0200

    Initial commit.

---
+++ XUnitTestProject2/XUnitTestProject2/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject2
+{
+    public class UnitTest1
+    {
+        // 1
+        [Theory]
+        [InlineData(1)]
+        public void Test1(int input)
+        // 2
+        //[Fact]
+        //public void Test1()
+        {
+
+        }
+    }
+}
